177065787dd51b89c6971f3c14356bab2bc869f2
pdhoward <patrick.howard@hotmail.com>
2018-05-14 14:16:02 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,8 @@
 ////////     routing master    ///////
 /////////////////////////////////////
 
-exports.sms =       require('./sms')
-exports.web =       require('./web')
 exports.auth =      require('./auth')
 exports.error =     require('./error')
 exports.help =      require('./help')
 exports.unk =       require('./unk')
-exports.db =        require('./db')
+exports.auth =      require('./log')
