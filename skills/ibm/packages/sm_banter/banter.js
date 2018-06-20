04268d3e7f84a589ded1c87e6a0ff269baf1bf89
pdhoward <patrick.howard@hotmail.com>
2018-06-20 11:08:18 -0400

    updates for response object

--- skills/ibm/packages/sm_banter/banter.js
+++ skills/ibm/packages/sm_banter/banter.js
@@ -30,30 +30,31 @@ function main(obj) {
               // platform. This action helps to set the state of the 'finite state machine'
 
 
-              m.updateWorkObj(obj)         // intialize work object with schema model
+              m.updateWorkObj(obj)         // intialize the machine with the data object
 
-              // a range of functions are now available for us to interrogate the object
+              // a range of functions are now available to interrogate the object
               // and perform other actions
 
               let args = m.getWorkObj()
-              // begin to construct the response object
+
+              // begin to capture key data to pass back as part of the response
               result.sender = args.message.From
               result.orgmessage = args
-              // get the agent response
+
+              // the reply array will capture the agent's responses to the text
               result.reply = []
 
               wat(args, (response) => {
                   result.reply = response.slice()
-                //  resolve(result)
 
-                // the reply of the agent must be recorded in the machine ... and
-                // the updated data object retreived and returned as the final step
+                // the response object must be recorded (validated) by the machine ... before
+                // the updated data object retrieved and returned as the final step
                 // of the microservice
 
-                  m.setAgentReply(result)
+                  m.setResponse(result)
                   let newObj = m.getWorkObj()
                   resolve(newObj)
-                  //return
+
               })
 
 
@@ -61,74 +62,6 @@ function main(obj) {
     })
   }
 
-//respond returns a string
-function respond(args, cb) {
-  let interactions = ["Hey, great to hear from you ",
-                      "This is text number ",
-                      "Todays date is ",
-                      "You said ",
-                      "I am here to sell you a toy. If you need to speak to a live agent, just say so",
-                      "Can I interest you in a toy? "]
-
-  let response = []
-  let msg = {
-    msg: ""
-  }
-  let newObj = {}
-
-  //let t = args.sequenceCnt
-  //let v = args.obj.dialogue.sequenceCnt
-  let t = 2
-  let v = 1
-  switch(t) {
-    case 0:
-      msg.msg = interactions[1] + t + " which seems a little low"
-      response.push(msg)
-      cb(response)
-      break;
-    case 1:
-      msg.msg = interactions[0] + "Guest"
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      msg.msg = interactions[4]
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      cb(response)
-      break;
-    case 2:
-      msg.msg = interactions[3] + "Default for right testing"
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      msg.msg = interactions[5]
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      cb(response)
-      break;
-    //
-    case 3:
-      msg.msg = interactions[1] + t + " which is a lot of texting"
-      response.push(msg)
-      cb(response)
-      break;
-    //
-    case 4:
-      msg.msg = interactions[2] + args.postdate
-      response.push(msg)
-      cb(response)
-      break;
-
-    default:
-      msg.msg = interactions[1] + t
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      msg.msg = "When will this discussion end?"
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      cb(response)
-      break;
-  }
-
-};
 //respond returns a string
 function wat(args, cb) {
   let interactions = ["hmmm. I didn't understand that. Please try again", "I am here to talk about toys", "Wat? I am stumped. ", "You said "]
